fede09f5a3cc00472f01bc206bf23de2eed75d9d
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-16 18:17:10 +0530

    Added react query config

--- CoinGeckoCryptoTracker/src/components/CoinTable/CoinTable.jsx
+++ CoinGeckoCryptoTracker/src/components/CoinTable/CoinTable.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { fetchCoinData } from "../../services/fetchCoinData";
+import { useQuery } from "react-query";
+
 function CoinTable() {
-  return <>Coin Table</>;
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isError, error } = useQuery(
+    ["coins", page],
+    () => fetchCoinData(page, "usd"),
+    {
+      retry: 2,
+      retryDelay: 1000,
+      cacheTime: 1000 * 60 * 2,
+    }
+  );
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      Coin Table <button onClick={() => setPage(page + 1)}>Click</button> {page}{" "}
+    </>
+  );
 }
 
 export default CoinTable;
--- CoinGeckoCryptoTracker/src/main.jsx
+++ CoinGeckoCryptoTracker/src/main.jsx
@@ -1,8 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-)
+  </QueryClientProvider>
+);
