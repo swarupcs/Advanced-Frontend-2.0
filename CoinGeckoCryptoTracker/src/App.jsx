18fe4636ca5c3e3940ee640402856d58bd7d703a
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-16 21:31:34 +0530

    Integrated zustand

--- CoinGeckoCryptoTracker/src/App.jsx
+++ CoinGeckoCryptoTracker/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+// import { useState } from 'react'
 import "./App.css";
 import Home from "./pages/Home";
-import { CurrencyContext } from "./context/CurrencyContext";
+// import { CurrencyContext } from './context/CurrencyContext';
 
 function App() {
-  const [currency, setCurrency] = useState("usd");
+  // const [currency, setCurrency] = useState('usd');
 
   return (
     <>
-      <CurrencyContext.Provider value={{ currency, setCurrency }}>
-        <Home />
-      </CurrencyContext.Provider>
+      {/* <CurrencyContext.Provider value={ { currency, setCurrency } }> */}
+      <Home />
+      {/* </CurrencyContext.Provider> */}
     </>
   );
 }
---
+++ CoinGeckoCryptoTracker/src/state/store.js
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+const store = create((set) => ({
+  currency: "usd",
+  setCurrency: (newCurrency) =>
+    set((state) => {
+      return {
+        ...state,
+        currency: newCurrency,
+      };
+    }),
+}));
+
+export default store;
