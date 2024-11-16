9710710e54ea2cd3524a2f0eda070a9d51e9623e
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-16 18:46:38 +0530

    Added coin table ui with cache

--- CoinGeckoCryptoTracker/src/services/fetchCoinData.js
+++ CoinGeckoCryptoTracker/src/services/fetchCoinData.js
@@ -6,8 +6,7 @@ export async function fetchCoinData(page = 1, currency = "usd") {
     const response = await axiosInstance.get(
       `/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`
     );
-    console.log(response);
-    return response;
+    return response.data;
   } catch (error) {
     console.error(error);
     return null;
