8c5b8e2053cc8cb083753bf218b2e49fc012316c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-17 18:05:52 +0530

    Fix: Added home page redirection from navbar

--- CoinGeckoCryptoTracker/src/components/Navbar/Navbar.jsx
+++ CoinGeckoCryptoTracker/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 // import { useContext } from "react";
 // import { CurrencyContext } from "../../context/CurrencyContext";
+import { useNavigate } from "react-router-dom";
 import currencyStore from "../../state/store";
 function Navbar() {
   const { setCurrency } = currencyStore();
+  const navigate = useNavigate();
+
+  function goToHome() {
+    navigate("/");
+  }
 
   return (
     <div className="navbar bg-base-100">
@@ -37,7 +43,7 @@ function Navbar() {
           </ul>
         </div>
       </div>
-      <div className="navbar-center">
+      <div onClick={goToHome} className="navbar-center">
         <a className="btn btn-ghost text-xl">Crpto Tracker</a>
       </div>
       <div className="navbar-end">
