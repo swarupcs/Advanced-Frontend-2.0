0ee2e8f4f0fb4110cbbddfe1f2aaeeb556d72d7c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-17 09:17:11 +0530

    Added layouts in router dom

--- CoinGeckoCryptoTracker/src/App.jsx
+++ CoinGeckoCryptoTracker/src/App.jsx
@@ -1,17 +1,10 @@
-// import { useState } from 'react'
 import "./App.css";
 import Routing from "./components/Routing/Routing";
-import Home from "./pages/Home";
-// import { CurrencyContext } from './context/CurrencyContext';
 
 function App() {
-  // const [currency, setCurrency] = useState('usd');
-
   return (
     <>
-      {/* <CurrencyContext.Provider value={ { currency, setCurrency } }> */}
       <Routing />
-      {/* </CurrencyContext.Provider> */}
     </>
   );
 }
--- CoinGeckoCryptoTracker/src/pages/Home.jsx
+++ CoinGeckoCryptoTracker/src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import Banner from "../components/Banner/Banner";
 import CoinTable from "../components/CoinTable/CoinTable";
-import Navbar from "../components/Navbar/Navbar";
 
 function Home() {
   return (
     <>
-      <Navbar />
       <Banner />
       <CoinTable />
     </>
