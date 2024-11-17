afb51d8249a946388f69d2a0f6bb59c88e0c3f2d
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-17 18:32:28 +0530

    Feat: Added code splitting with lazy loading and suspense

--- CoinGeckoCryptoTracker/src/components/Routing/Routing.jsx
+++ CoinGeckoCryptoTracker/src/components/Routing/Routing.jsx
@@ -1,19 +1,34 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../../pages/Home";
-import CoinDetailsPage from "../../pages/CoinDetailsPage";
+import { lazy, Suspense } from "react";
 import MainLayout from "../../pages/Layout";
+import PageLoader from "../PageLoader/PageLoader";
+
+const Home = lazy(() => import("../../pages/Home"));
+const CoinDetailsPage = lazy(() => import("../../pages/CoinDetailsPage"));
 
 function Routing() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/details/:coinId" element={<CoinDetailsPage />} />
+        <Route
+          index
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/details/:coinId"
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <CoinDetailsPage />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
 }
 
 export default Routing;
-
-
