42f2b06331c9b69a8eba1791755ee55d3e483b9e
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-11-17 19:17:13 +0530

    Feat: added error boundary

--- CoinGeckoCryptoTracker/src/components/Routing/Routing.jsx
+++ CoinGeckoCryptoTracker/src/components/Routing/Routing.jsx
@@ -2,32 +2,35 @@ import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import MainLayout from "../../pages/Layout";
 import PageLoader from "../PageLoader/PageLoader";
+import CustomErrorBoundary from "../CustomErrorBoundary/CustomErrorBoundary";
 
 const Home = lazy(() => import("../../pages/Home"));
 const CoinDetailsPage = lazy(() => import("../../pages/CoinDetailsPage"));
 
 function Routing() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route
-          index
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <Home />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/details/:coinId"
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <CoinDetailsPage />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
+    <CustomErrorBoundary>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route
+            index
+            element={
+              <Suspense fallback={<PageLoader />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/details/:coinId"
+            element={
+              <Suspense fallback={<PageLoader />}>
+                <CoinDetailsPage />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </CustomErrorBoundary>
   );
 }
 
