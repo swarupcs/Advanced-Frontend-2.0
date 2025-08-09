39fd269479b08200f332cfde580016af82a94f65
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-08-09 19:14:38 +0530

    commit change

--- CoinGeckoCryptoTracker/src/helpers/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { COINGECKO_API_URL } from "./constants";
-
-const axiosInstance = axios.create({
-  baseURL: COINGECKO_API_URL,
-});
-
-export default axiosInstance;
